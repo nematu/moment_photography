d77ebbaa1bdd052ca9605b2aebc13ab70c24dccd
nematu <nematullah_ayaz@yahoo.com>
2023-03-01 19:40:09 -0800

    added index.php file

---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+/*
+ * authors: Nematu Ayaz
+ * index.php
+ */
+
+//error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//session start
+session_start();
+
+require_once('vendor/autoload.php');
+//require $_SERVER['DOCUMENT_ROOT'].'/../config.php';
+require_once ('config.php');
+
+
+
+//instance of the base class
+$f3 = Base::instance();
+$f3->set('DEBUG', 3);
+
+//instantiate needed classes
+$dataLayer = new Data_Layer($dbh);
+$validator = new CapturedMomentsValidator();
+$controller = new CapturedMomentsController($f3);
+
+//default route (home page)
+$f3->route('GET|POST /', function () {
+    global $controller;
+    $controller->home();
+});
+$f3->route('GET|POST /home', function () {
+    global $controller;
+    $controller->home();
+});
+
+// Packages Route
+$f3->route('GET /packages', function() {
+    global $controller;
+    $controller->packages();
+});
+
+
+//Define an order route
+$f3->route('GET|POST /connect', function() {
+    global $controller;
+    $controller->connect();
+});
+
+//Define a wedding route
+$f3->route('GET|POST /wedding', function() {
+    global $controller;
+    $controller->wedding();
+});
+
+//Define a family route
+$f3->route('GET|POST /family', function() {
+    global $controller;
+    $controller->family();
+});
+
+//Define a summary route
+$f3->route('POST|GET /summary', function() {
+    global $controller;
+    $controller->summary();
+    session_destroy();
+});
+
+// admin
+$f3->route('GET /admin', function() {
+    global $controller;
+    $controller->admin();
+});
+
+$f3->run();
